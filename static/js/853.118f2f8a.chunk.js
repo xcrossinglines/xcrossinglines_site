2602306662d9bbb951c7ada9f4d5caba41ca5d7a
Freddy Smith <sdumosmith@gmail.com>
2023-08-08 22:14:28 +0200

    version 1.9

---
+++ static/js/853.118f2f8a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkxcrossinglines=self.webpackChunkxcrossinglines||[]).push([[853],{3853:function(t,n,r){r.d(n,{vPQ:function(){return a}});var e=r(9983);function a(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"}},{tag:"path",attr:{d:"M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"}}]})(t)}}}]);
+//# sourceMappingURL=853.118f2f8a.chunk.js.map
